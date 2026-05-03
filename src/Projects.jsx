a357c6afe51481fd0a7156c10424565ada184369
Saoui_Yassir <yassirsaoui06@gmail.com>
2026-05-03 15:21:05 +0100

    Adding Real Cards

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,15 +1,26 @@
 import Card from "./Card";
+import './Projects.css'
+import pic1 from './assets/prj1.png'
+import pic2 from './assets/prj2.png'
 
 function Projects() {
     return (
-        <section>
-            <h1>See my works</h1>
-            <Card 
-                title= "Title"
-                srcImg= "https://picsum.photos/300/200"
-                desc= "Description"
-                link= "https://instagram.com"
-            />
+        <section className="prj-section">
+            <h1 className="tile-prj">See my works</h1>
+            <div className="cards">
+                <Card 
+                    title= "LuxAuto Parts & Services"
+                    srcImg= {pic1}
+                    desc= "Static E-commerce website selling spare parts for luxury cars using HTML | CSS | JS."
+                    link= "https://saouiyassir.github.io/Web_Projet/"
+                />
+                <Card 
+                    title= "Mini Game C++"
+                    srcImg= {pic2}
+                    desc= "A polished 2D mini-game built with C++ and SFML 2, featuring smooth animations, collisions, health UI, and scalable OOP architecture."
+                    link= "https://github.com/SaouiYassir/miniGame_cpp"
+                />
+            </div>
         </section>
     );
 }
