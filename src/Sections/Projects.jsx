2d9dc608dea94ec7f6c1ca3a5c6506f460927d34
Saoui_Yassir <yassirsaoui06@gmail.com>
2026-06-11 17:44:00 +0100

    Adding the portfolio card in Projects

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -2,6 +2,7 @@ import Card from "../Components/Card/Card.jsx";
 import './Projects.css'
 import pic1 from '../assets/prj1.png'
 import pic2 from '../assets/prj2.png'
+import pic3 from '../assets/prj3.png';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -12,6 +13,14 @@ function Projects() {
         <section className="prj-section" id="Projects">
             <h1 className="tile-prj" data-aos="fade-up" data-aos-anchor-placement="center-bottom">Featured Projects</h1>
             <div className="cards">
+                <Card
+                    title= "Personal Portfolio"
+                    srcImg= {pic3}
+                    desc= "A modern and responsive personal portfolio built with React and Tailwind CSS, featuring a clean UI, seamless navigation, and a structured layout to showcase frontend projects and professional skills."
+                    techs={['JS', 'CSS', 'React', 'Git']}
+                    link_github= "https://github.com/SaouiYassir/SaouiYassir.github.io"
+                    link_demo= "https://saouiyassir.github.io/"
+                />
                 <Card 
                     title= "LuxAuto Parts & Services"
                     srcImg= {pic1}
@@ -24,7 +33,7 @@ function Projects() {
                     title= "Mini Game C++"
                     srcImg= {pic2}
                     desc= "A polished 2D mini-game built with C++ and SFML 2, featuring smooth animations, collisions, health UI, and scalable OOP architecture."
-                    techs={['C++', 'POO', 'SFML 2', 'Git']}
+                    techs={['C++', 'OOP', 'SFML 2', 'Git']}
                     link_github= "https://github.com/SaouiYassir/miniGame_cpp"
                     link_demo= "https://github.com/SaouiYassir/miniGame_cpp"
                 />
