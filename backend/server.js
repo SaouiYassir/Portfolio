f0acd17e2829bb5ffb68dfc53c017c6d1ab4a627
YassirSaoui <yassirsaoui06@gmail.com>
2026-09-15 01:29:54 +0100

    Creating the server side

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Your server is running successfully...")
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Your server is in http://localhost:${port}`);
+});
