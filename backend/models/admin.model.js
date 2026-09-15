b44d27aa37ade337d8ab1f04f8fd2d0d24cd3d36
YassirSaoui <yassirsaoui06@gmail.com>
2026-09-15 12:31:52 +0100

    Creating admins structure

---
+++ backend/models/admin.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+const adminSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    }, {
+        timestamps: true
+    }
+);
+
+export const user = mongoose.model("User", adminSchema);
