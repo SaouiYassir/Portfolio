50e1f0db3d2880371d50908c0fb8615394210da5
YassirSaoui <yassirsaoui06@gmail.com>
2026-09-22 14:29:03 +0000

    Adding the get project by id function

--- backend/controllers/project.controllers.js
+++ backend/controllers/project.controllers.js
@@ -65,3 +65,30 @@ export const listProjects = async (req, res) => {
     });
   }
 };
+
+
+export const getProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const foundProject = await project.findById(id);
+    if (!foundProject) {
+      return res.status(404).json({
+        success: false,
+        message: "Project not found.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: foundProject,
+    });
+  } catch (error) {
+    console.error("Error fetching project:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+      error: error.message,
+    });
+  }
+};
--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,13 +1,15 @@
 import express from "express"
 import {
   addProject,
-  listProjects
+  listProjects,
+  getProject
 } from "../controllers/project.controllers.js"
 
 const projectRouter = express.Router()
 
 projectRouter.post("/project", addProject)          
 projectRouter.get("/projects", listProjects)        
+projectRouter.get("/project/:id", getProject)
 
 
 export default projectRouter
